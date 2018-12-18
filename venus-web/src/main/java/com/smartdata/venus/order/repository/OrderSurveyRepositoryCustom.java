62a51d4ee6fa22da6d3ecb01df4e21af43a95bce
xuliang <546382065@qq.com>
2018-12-18 21:55:20 +0800

    订单提交

---
+++ venus-web/src/main/java/com/smartdata/venus/order/repository/OrderSurveyRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.smartdata.venus.order.repository;
+
+import com.smartdata.venus.order.domain.OrderSurvey;
+
+
+/**
+ * @author xuliang
+ * @date 2018/12/18
+ */
+public interface OrderSurveyRepositoryCustom {
+	
+}
+
+
