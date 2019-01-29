d242c0ab5bba330eda729b4f4a9d03aeb23170e4
lookhua <2560636051@qq.com>
2019-01-29 20:23:14 +0800

    init

--- venus-uc/venus-uc-service/src/main/java/com/smartdata/venus/uc/system/repository/FileRepository.java
+++ venus-uc/venus-uc-service/src/main/java/com/smartdata/venus/uc/system/repository/FileRepository.java
@@ -1,5 +1,6 @@
 package com.smartdata.venus.uc.system.repository;
 
+import com.smartdata.core.jpa.BaseRepository;
 import com.smartdata.venus.uc.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,10 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author khlu
  * @date 2018/11/02
  */
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository extends BaseRepository<File, Long> {
 
     /**
      * 查找指定文件sha1记录
+     *
      * @param sha1 文件sha1值
      */
     public File findBySha1(String sha1);
